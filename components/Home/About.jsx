1d8977b205ef4e94b64540e295f7c897df457cba
kevinokello <okellokevin77@gmail.com>
2022-09-07 13:55:38 +0300

    about me

--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -21,27 +21,26 @@ const About = () => {
         <div className="flex sm:flex-row justify-center flex-col px-4">
           <div className="w-full sm:w-1/2 mb-12 sm:mb-0">
             <h1 className=" capitalize title-font  sm:text-3xl text-2xl mb-2 font-bold">
-              <span className="text-primary">People </span>
-              ignore designs
-              <br />
-              that ignores
-              <span className="text-primary ml-2">People </span>
+              <span className="text-primary">“Simplicity </span>
+              is the soul of
+              <span className="text-primary ml-2">efficiency” </span>
             </h1>
             <p className=" capitalize leading-relaxed text-xs md:text-sm text-textSecondary dark:text-textDarkSecondary">
-              - Frank Chimero
+              - Austin Freeman
             </p>
           </div>
           <div className="w-full sm:w-1/2">
+            <p className="leading-relaxed text-textSecondary text-xs md:text-sm dark:text-textDarkSecondary">
+              Shortly about me:On free time I like playing Table tennis
+              immensely, so if you want to join I am waiting for you ; As a Jack
+              of All Trades, I'm curious to learn more about new technologies.{" "}
+              <br />
+              Always woke and motivated to drive meaningful levels of impact
+              across the globe.
+            </p>
             <h1 className="capitalize title-font sm:text-2xl text-xl mb-2 font-semibold">
               Connect With Me
             </h1>
-            <p className="leading-relaxed text-textSecondary text-xs md:text-sm dark:text-textDarkSecondary">
-              Shortly about me:On free time I like playing Table tennis
-              immensely, so if you want to join I am waiting for you ; As a
-              Jack of All Trades, I'm curious to learn more about new
-              technologies. <br></br>Always woke and motivated to drive meaningful levels
-              of impact across the globe.
-            </p>
             <div className="select-none capitalize flex my-4">
               {socialLinks.map(({ icon, url }, index) => (
                 <Link href={url} key={index}>
