91d90156d6ec79b5834beb1bfb7010134fc74396
kevinokello <okellokevin77@gmail.com>
2022-09-07 12:54:01 +0300

    hero image

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -6,21 +6,26 @@ import { HiDownload, HiEye } from "react-icons/hi"
 
 const Hero = () => {
   return (
-    <Section id="intro" className="relative !h-full !py-10 md:!py-20 overflow-x-clip">
+    <Section
+      id="intro"
+      className="relative !h-full !py-10 md:!py-20 overflow-x-clip"
+    >
       <div className="flex flex-row justify-between items-center flex-wrap-reverse p-2">
         <div className="flex-grow text-center lg:text-start  w-1/2">
           <h1 className="title-font sm:text-4xl text-2xl mb-2 font-bold ">
             Hi 👋, I Am Kevin Okello
-            </h1>
-            <h3 className="text-lg mt-1 opacity-90 font-normal capitalize ">
-          A Full Stack Web Developer
-            </h3>
-            <div className="w-24 h-1 bg-primary bg-opacity-70 mt-6 mb-4">
-              <div className="w-10 h-1 bg-primary"></div>
-            </div>
+          </h1>
+          <h3 className="text-lg mt-1 opacity-90 font-normal capitalize ">
+            A Full Stack Web Developer
+          </h3>
+          <div className="w-24 h-1 bg-primary bg-opacity-70 mt-6 mb-4">
+            <div className="w-10 h-1 bg-primary"></div>
+          </div>
 
           <p className="mb-6 mt-2 xl:w-[80%] leading-relaxed text-textSecondary font-medium dark:text-textDarkSecondary opacity-70 text-xs md:text-sm">
-            Lorem ipsum dolor, sit amet consectetur apsum animi! Minima modi pariatur, delectus asperiores aspernatur magnam voluptates soluta tempora accusantium?
+            Lorem ipsum dolor, sit amet consectetur apsum animi! Minima modi
+            pariatur, delectus asperiores aspernatur magnam voluptates soluta
+            tempora accusantium?
           </p>
           <div className="flex flex-wrap justify-center lg:justify-start">
             <div>
@@ -35,7 +40,14 @@ const Hero = () => {
               </a>
             </div>
             <div>
-              <button onClick={() => document.querySelector("#work").scrollIntoView({ behavior: "smooth" })} className="relative inline-flex items-center  justify-center p-0.5 mb-3 mr-2 overflow-hidden text-xs md:text-sm font-medium  rounded-lg group bg-gradient-to-br from-primary/80 via-secondary/60 to-secondary/80 group-hover:from-primary group-hover:via-secondary/80 group-hover:to-secondary hover:text-white dark:text-white outline-none">
+              <button
+                onClick={() =>
+                  document
+                    .querySelector("#work")
+                    .scrollIntoView({ behavior: "smooth" })
+                }
+                className="relative inline-flex items-center  justify-center p-0.5 mb-3 mr-2 overflow-hidden text-xs md:text-sm font-medium  rounded-lg group bg-gradient-to-br from-primary/80 via-secondary/60 to-secondary/80 group-hover:from-primary group-hover:via-secondary/80 group-hover:to-secondary hover:text-white dark:text-white outline-none"
+              >
                 <span className="relative text-primary flex justify-center items-center hover:text-white px-5 py-2.5 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   <HiEye className="mr-2 w-5 h-5 transition-none" />
                   View Projects
@@ -50,11 +62,10 @@ const Hero = () => {
             alt="hero"
             width={550}
             height={400}
-            src="/images/me.jpeg"
+            src="https://avatars.githubusercontent.com/u/76564523?v=4"
           />
         </div>
       </div>
-
     </Section>
   );
 };
