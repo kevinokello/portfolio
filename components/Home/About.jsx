3a83e2eefcf8afa976426a55450c081c07379576
kevinokello <okellokevin77@gmail.com>
2022-09-07 13:51:05 +0300

    about description

--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -17,41 +17,44 @@ const About = () => {
       },
     ];
     return (
-        <Section id="about" className="!relative">
-            <div className='flex sm:flex-row justify-center flex-col px-4'>
-                <div className='w-full sm:w-1/2 mb-12 sm:mb-0'>
-                    
-                    <h1 className=" capitalize title-font  sm:text-3xl text-2xl mb-2 font-bold">
-                        <span className='text-primary'>People </span>
-                        ignore designs
-                        <br />
-                        that ignores
-                        <span className='text-primary ml-2'>People </span>
-                    </h1>
-                    <p className=" capitalize leading-relaxed text-xs md:text-sm text-textSecondary dark:text-textDarkSecondary">
-                        - Frank Chimero
-                    </p>
-                </div>
-                <div className='w-full sm:w-1/2'>
-                    <h1 className="capitalize title-font sm:text-2xl text-xl mb-2 font-semibold">
-                        Connect With Me
-                    </h1>
-                    <p className="leading-relaxed text-textSecondary text-xs md:text-sm dark:text-textDarkSecondary">
-                        I am approximately on every social media platform. You can also mail me anytime, I will reply within 24 hours
-                    </p>
-                    <div className='select-none capitalize flex my-4'>
-                        {socialLinks.map(({ icon, url }, index) => (
-                            <Link href={url} key={index}>
-                                <div className='border-primary border-2 mr-3 mb-3 rounded-full p-2 text-primary hover:bg-primary hover:text-white transition duration-300 cursor-pointer'>
-                                    {icon}
-                                </div>
-                            </Link>
-                        ))}
-                    </div>
-                </div>
+      <Section id="about" className="!relative">
+        <div className="flex sm:flex-row justify-center flex-col px-4">
+          <div className="w-full sm:w-1/2 mb-12 sm:mb-0">
+            <h1 className=" capitalize title-font  sm:text-3xl text-2xl mb-2 font-bold">
+              <span className="text-primary">People </span>
+              ignore designs
+              <br />
+              that ignores
+              <span className="text-primary ml-2">People </span>
+            </h1>
+            <p className=" capitalize leading-relaxed text-xs md:text-sm text-textSecondary dark:text-textDarkSecondary">
+              - Frank Chimero
+            </p>
+          </div>
+          <div className="w-full sm:w-1/2">
+            <h1 className="capitalize title-font sm:text-2xl text-xl mb-2 font-semibold">
+              Connect With Me
+            </h1>
+            <p className="leading-relaxed text-textSecondary text-xs md:text-sm dark:text-textDarkSecondary">
+              Shortly about me:On free time I like playing Table tennis
+              immensely, so if you want to join I am waiting for you ; As a
+              Jack of All Trades, I'm curious to learn more about new
+              technologies. <br></br>Always woke and motivated to drive meaningful levels
+              of impact across the globe.
+            </p>
+            <div className="select-none capitalize flex my-4">
+              {socialLinks.map(({ icon, url }, index) => (
+                <Link href={url} key={index}>
+                  <div className="border-primary border-2 mr-3 mb-3 rounded-full p-2 text-primary hover:bg-primary hover:text-white transition duration-300 cursor-pointer">
+                    {icon}
+                  </div>
+                </Link>
+              ))}
             </div>
-        </Section>
-    )
+          </div>
+        </div>
+      </Section>
+    );
 }
 
 export default About
