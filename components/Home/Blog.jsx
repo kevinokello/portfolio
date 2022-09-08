1c1eddbf26466accdadcd41ac6e3cfbd6cf932a0
kevinokello <okellokevin77@gmail.com>
2022-09-08 09:51:24 +0300

    blog

--- components/Home/Blog.jsx
+++ components/Home/Blog.jsx
@@ -4,57 +4,57 @@ import Section from '../Customs/Section'
 import BlogCard from '../Customs/BlogCard'
 import { HiOutlineArrowRight, HiOutlineEye } from 'react-icons/hi'
 
-const Blog = () => {
-  const categories = [
-    { name: "Web3", slug: "web3" },
-    { name: "Crypto Currency", slug: "crypto-currency" },
-    { name: "Daily News", slug: "daily-news" },
+// const Blog = () => {
+//   const categories = [
+//     { name: "Web3", slug: "web3" },
+//     { name: "Crypto Currency", slug: "crypto-currency" },
+//     { name: "Daily News", slug: "daily-news" },
 
-  ]
-  const articles = [
-    {
-      thumbnail: "/images/blog/blog-1.jpg",
-      title: "Best Computer setup for programming under ₹10,000",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam minima omn.",
-      categories: [categories[2]],
-      authorName: "admin",
-      timeToRead: "4 minutes",
-      publishedAt: "26 august 2022"
-    },
-    {
-      thumbnail: "/images/blog/blog-2.jpg",
-      title: "Microsoft New Windows 11 is Amazing",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam minima omnis dolorem, excepturi autem.",
-      categories: [categories[1], categories[2]],
-      authorName: "admin",
-      timeToRead: "8 minutes",
-      publishedAt: "20 may 2022"
-    },
-    {
-      thumbnail: "/images/blog/blog-3.jpg",
-      title: "Web 2.0 is better than web 2.0",
-      description: "Lorem, ipsum dolor elit. Nostrum ipsam minima omn, nostrum ipsam minima omn.",
-      categories: [categories[0], categories[2]],
-      authorName: "admin",
-      timeToRead: "6 minutes",
-      publishedAt: "28 april 2022"
-    },
-  ]
-  return (
-    // <Section
-    //   id="blog"
-    //   title="My Blog"
-    //   description="Welcome to my digital garden where I share what I'm learning, my shipped great products, becoming a better developer and growing a career in tech."
-    //   subtitle="Recent Posts"
-    // >
-    //   <div className="flex flex-wrap justify-center items-start">
-    //     {articles.map((article, index) => (
-    //       <BlogCard key={index} {...article} />
-    //     ))}
-    //   </div>
+//   ]
+//   const articles = [
+//     {
+//       thumbnail: "/images/blog/blog-1.jpg",
+//       title: "Best Computer setup for programming under ₹10,000",
+//       description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam minima omn.",
+//       categories: [categories[2]],
+//       authorName: "admin",
+//       timeToRead: "4 minutes",
+//       publishedAt: "26 august 2022"
+//     },
+//     {
+//       thumbnail: "/images/blog/blog-2.jpg",
+//       title: "Microsoft New Windows 11 is Amazing",
+//       description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam minima omnis dolorem, excepturi autem.",
+//       categories: [categories[1], categories[2]],
+//       authorName: "admin",
+//       timeToRead: "8 minutes",
+//       publishedAt: "20 may 2022"
+//     },
+//     {
+//       thumbnail: "/images/blog/blog-3.jpg",
+//       title: "Web 2.0 is better than web 2.0",
+//       description: "Lorem, ipsum dolor elit. Nostrum ipsam minima omn, nostrum ipsam minima omn.",
+//       categories: [categories[0], categories[2]],
+//       authorName: "admin",
+//       timeToRead: "6 minutes",
+//       publishedAt: "28 april 2022"
+//     },
+//   ]
+//   return (
+//     <Section
+//       id="blog"
+//       title="My Blog"
+//       description="Welcome to my digital garden where I share what I'm learning, my shipped great products, becoming a better developer and growing a career in tech."
+//       subtitle="Recent Posts"
+//     >
+//       <div className="flex flex-wrap justify-center items-start">
+//         {articles.map((article, index) => (
+//           <BlogCard key={index} {...article} />
+//         ))}
+//       </div>
 
-    // </Section>
-  );
-}
+//     </Section>
+//   );
+// }
 
 export default Blog
